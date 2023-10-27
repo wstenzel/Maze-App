6cc6e7528c688c8552d2db3ea15dd635f9d22227
Will Stenzel <wstenzel@stu.naperville203.org>
2023-10-27 14:10:31 -0500

    gtrbvwnb

---
+++ src/SquareTest.java
@@ -0,0 +1,12 @@
+
+public class SquareTest
+{
+    public static void main(String[] args)
+    {
+        Square test = new Square(3,4,1);
+        System.out.println(test);
+        System.out.println("expected: 3 4 1 #");
+        
+
+    }
+}
